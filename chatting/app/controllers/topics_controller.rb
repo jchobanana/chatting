b7ea13ad5bcb5a1cd57346700d854dff7202e85e
J.CHO <jchobanana@gmail.com>
2016-03-28 21:05:38 +0800

    bootstrap and new, edit

--- chatting/app/controllers/topics_controller.rb
+++ chatting/app/controllers/topics_controller.rb
@@ -9,7 +9,26 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
   end
 
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new( topic_params )
+    @topic.user = current_user
+    if @topic.save
+      redirect_to topic_url(@topic)
+    end
+  end
+
   def display_name
     email.split('@').first
   end
+
+  protected
+
+  def topic_params
+    params.require(:topic).permit(:content, :subject, :image)
+  end
+
 end
