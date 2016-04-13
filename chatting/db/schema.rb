b313b220a2c6e8402c442e05659ec3e2f81ee0b8
J.CHO <jchobanana@gmail.com>
2016-04-13 16:39:59 +0800

    spec

--- chatting/db/schema.rb
+++ chatting/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409152045) do
+ActiveRecord::Schema.define(version: 20160412031845) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 20160409152045) do
     t.string   "place"
     t.string   "style"
     t.string   "story"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
